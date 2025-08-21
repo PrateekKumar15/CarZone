81044f3e9180342e5b822c51c1898799826d2a64
Prateek Kumar <prateekkumar72007@gmail.com>
2025-08-21 23:59:34 +0530

    Implement GetEngineByID method in EngineHandler with error handling and JSON response

--- handler/engine/engine.go
+++ handler/engine/engine.go
@@ -1,1 +1,51 @@
 package engine
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/PrateekKumar15/CarZone/service"
+	"encoding/json"
+	"github.com/gorilla/mux"
+)	
+
+type EngineHandler struct {
+	service  service.EngineServiceInterface
+}
+func NewEngineHandler(service service.EngineServiceInterface) *EngineHandler {
+	return &EngineHandler{service: service}
+}
+
+func (h *EngineHandler) GetEngineByID(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	vars := mux.Vars(r)
+	id := vars["id"]
+	if id == "" {
+		http.Error(w, "ID is required", http.StatusBadRequest)
+		return
+	}
+
+	engine, err := h.service.GetEngineByID(ctx, id)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error fetching engine: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	if engine == nil {
+		http.Error(w, "Engine not found", http.StatusNotFound)
+		return
+	}
+	body,err := json.Marshal(engine)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error marshalling response: %v", err), http.StatusInternalServerError)
+		return
+	}
+w.Header().Set("Content-Type", "application/json")
+w.WriteHeader(http.StatusOK)
+	_,_ = w.Write(body)	
+
+}
+
+
+
+
+
